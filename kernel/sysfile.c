0ae52c8c5764a5460ff4c50b3d2e173d02128200
bigboss2063 <bigboss2063@outlook.com>
2023-04-26 11:06:41 +0800

    symbolic links.

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -304,11 +304,29 @@ sys_open(void)
       return -1;
     }
   } else {
-    if((ip = namei(path)) == 0){
-      end_op();
-      return -1;
+    int symlinkdepth = 0;
+    while (1) {
+      symlinkdepth++;
+      if (symlinkdepth > 10) {
+        end_op();
+        return -1;
+      }
+      if((ip = namei(path)) == 0){
+        end_op();
+        return -1;
+      }
+      ilock(ip);
+      if (ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {
+        if(readi(ip, 0, (uint64)path, 0, MAXPATH) < 0) {
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+        iunlockput(ip);
+      } else {
+        break;
+      }
     }
-    ilock(ip);
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
@@ -482,5 +500,34 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void) {
+  char path[MAXPATH], target[MAXPATH];
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0) {
+    return -1;
+  }
+
+  begin_op();
+
+  struct inode *ip;
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0) {
+    end_op();
+    return -1;
+  }
+
+  int n = strlen(target);
+
+  if(writei(ip, 0, (uint64)target, 0, n) < 0) {
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
+
   return 0;
 }
